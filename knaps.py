b5cee4e3320b4091990f849219ee560e7bf22137
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-04-11 20:45:02 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -136,6 +136,7 @@ def main():
                 accuracy = float(lines[5].split()[1]) * 100
                 precision = float(lines[2].split()[1]) * 100
                 recall = float(lines[3].split()[1]) * 100
+                f1 = float(lines[4].split()[1]) * 100
         
                 # Display the metrics
                 html_code = f"""
