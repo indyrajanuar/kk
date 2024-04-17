b900271b25e29055162f101009f6b7cd8201461e
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-04-17 16:37:18 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -41,7 +41,7 @@ def split_data(data):
 
 def load_model():
     # Load pre-trained ERNN model
-    model = keras.models.load_model('model_fold_3.h5')
+    model = keras.models.load_model('model_fold_4.h5')
     return model
 
 def ernn(data, model):
