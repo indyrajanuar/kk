5229b821187c3a64d884c56ee48a0a6aa56ca5b9
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-04-29 15:45:32 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -93,6 +93,7 @@ def main():
         st.markdown('<h3 style="text-align: left;"> Hipertensi </h1>', unsafe_allow_html=True)
         st.write('Selamat Datang di Website Data Penyakit Hipertensi menggunakan Metode Elman Recurrent Neural Network')
         st.write('Hipertensi (Tekanan Darah Tinggi) merupakan kondisi dimana terjadinya peningkatan tekanan darah sistolik ≥ 140 mmHg atau diastolik ≥ 90 mmHg. Hipertensi biasa disebut dengan “the silent killer”, dikarenakan kebanyakan penderitanya tidak sadar dirinya mengidap hipertensi, dan baru menyadari ketika telah terjadinya komplikasi. Dan diketahui bahwa dari penderita hipertensi, hanya sepertiga atau 36.8% dari penderita hipertensi yang terdiagnosa oleh tenaga medis dan sekitar hanya 0.7% yang meminum obat.')
+        st.write('Elman Recurrent Neural Network adalah jenis Jaringan Syaraf Tiruan yang memperkenalkan lapisan tambahan yang dikenal sebagai lapisan konteks. Neuron konteks berperan dalam menyimpan informasi dari neuron hidden, yang akan digunakan bersamaan dengan data input dalam perhitungan fungsi pembelajaran')
         st.markdown('<h3 style="text-align: left;"> View Data </h1>', unsafe_allow_html=True)
         if upload_file is not None:
             df = pd.read_csv(upload_file)
