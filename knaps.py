4105b3ba4952fc95a7f0c58bb86ef4de68812b74
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-04-29 15:51:27 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -109,8 +109,8 @@ def main():
             st.dataframe(df)
             
             st.markdown('<h3 style="text-align: left;"> Melakukan Cleaning Data </h1>', unsafe_allow_html=True)
+            st.write('Pada bagian ini melakukan pembersihan dataset yang tidak memiliki relevansi terhadap faktor risiko pada penyakit hipertensi, seperti menghapus satuan yang tidak diperlukan.')
             if st.button("Clean Data"):
-                st.write('Pada bagian ini melakukan pembersihan dataset yang tidak memiliki relevansi terhadap faktor risiko pada penyakit hipertensi, seperti menghapus satuan yang tidak diperlukan.')
                 df_cleaned = clean_data(df)
                 st.write("Data cleaning completed.")
                 st.dataframe(df_cleaned)
