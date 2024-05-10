4fe5020bd23e041483aa05737be72a08a8dd8e71
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-05-10 15:45:48 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -267,6 +267,7 @@ def main():
         # Input fields
         with st.form("my_form"):
             with st.container():
+                col1, col2 = st.columns(2)  # Split the layout into two columns
                 with st.column():
                     age = st.number_input("Umur", min_value=0, max_value=150, step=1, value=30)
                     bmi = st.number_input("IMT", min_value=0.0, max_value=100.0, step=0.1, value=25.0)
