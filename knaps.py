77f1222921bf467d7687b6b8a410896cf6c5b43d
indyrajanuar <84507394+indyrajanuar@users.noreply.github.com>
2024-07-23 21:47:09 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -248,27 +248,27 @@ def main():
                 
             st.markdown(html_code, unsafe_allow_html=True)
 
-            # Plot training & validation accuracy values
-            plt.figure(figsize=(12, 4))
-            plt.subplot(1, 2, 1)
-            plt.plot(history.history['accuracy'], label='Train Accuracy')
-            plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
-            plt.title('Model Accuracy')
-            plt.xlabel('Epoch')
-            plt.ylabel('Accuracy')
-            plt.legend(loc='upper left')
+            # # Plot training & validation accuracy values
+            # plt.figure(figsize=(12, 4))
+            # plt.subplot(1, 2, 1)
+            # plt.plot(history.history['accuracy'], label='Train Accuracy')
+            # plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+            # plt.title('Model Accuracy')
+            # plt.xlabel('Epoch')
+            # plt.ylabel('Accuracy')
+            # plt.legend(loc='upper left')
         
-            # Plot training & validation loss values
-            plt.subplot(1, 2, 2)
-            plt.plot(history.history['loss'], label='Train Loss')
-            plt.plot(history.history['val_loss'], label='Validation Loss')
-            plt.title('Model Loss')
-            plt.xlabel('Epoch')
-            plt.ylabel('Loss')
-            plt.legend(loc='upper left')
+            # # Plot training & validation loss values
+            # plt.subplot(1, 2, 2)
+            # plt.plot(history.history['loss'], label='Train Loss')
+            # plt.plot(history.history['val_loss'], label='Validation Loss')
+            # plt.title('Model Loss')
+            # plt.xlabel('Epoch')
+            # plt.ylabel('Loss')
+            # plt.legend(loc='upper left')
         
-            st.pyplot(plt.gcf())  # Pass the current figure to st.pyplot()        
-            plt.clf()  # Clear the current plot to avoid displaying it multiple times
+            # st.pyplot(plt.gcf())  # Pass the current figure to st.pyplot()        
+            # plt.clf()  # Clear the current plot to avoid displaying it multiple times
 
             # # Membuat DataFrame untuk menampilkan x_test, prediksi vs aktual
             # comparison_df = x_test.copy()
